e5ef8dfbe32c8d5c02d970bc3fcf59f9fc7b77f1
unknown <framin85@gmail.com>
2022-10-14 18:02:06 +0300

    Object model message

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 token = process.env.TOKEN;
-const bot = new TelegramBot(token,{polling:true});
+const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message',(msg)=>{
-   lightBuh(msg.text);
-   console.log(msg.text)
-    
+bot.on('message', (msg) => {
+
+    lightBuh(msg.text, msg.from.id);
+
 })
-function lightBuh(someText){
-    let debet = someText.match(/\+\d+/g);
-    console.log(debet);
 
+function lightBuh(someText, id) {
+    let debet, credit = ''
+    let coins = (someText.match(/\+\d+/g) != null) ? someText.match(/\+\d+/g)[0] :
+        (someText.match(/\-\d+/g) != null) ? someText.match(/\-\d+/g)[0] : (answerBot(Error, id), 0)
+    coins[0] === '-' ? credit = +coins : debet = +coins;
+    let description = someText.match(/[A-ZА-Яії ]+/gi)[0];
+    console.log(description);
 }
+
+function answerBot(message, id) {
+    bot.sendMessage(id, message)
+}
+const Error = "Вкажіть будь ласка це доходи чи витрати"
